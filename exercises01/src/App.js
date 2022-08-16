d03d159d835b4e3d9b2589dd9c7ec6000153bd8f
unknown <vladancupric@gmail.com>
2022-08-16 21:58:12 +0200

    New commits new file feedbackdata

--- exercises01/src/App.js
+++ exercises01/src/App.js
@@ -2,36 +2,11 @@ import {useState} from "react"
 import './App.css';
 import Header from './components/Header';
 import FeedbackItem from './components/FeedbackItem';
+import FeedbackData from "./data/FeedbackData";
 
 function App() {
 
-  const[feedback, setFeedback] = useState(
-    {
-    id:1,
-    rating:10,
-    text:"Alert is only used on the route moving to that route we can prevent "
-  },
-  {
-    id:2,
-    rating:6,
-    text:"Alert is only used on the route moving to that route we can prevent "
-  },
-  {
-    id:3,
-    rating:0,
-    text:"Alert is only used on the route moving to that route we can prevent "
-  },
-  {
-    id:4,
-    rating:7,
-    text:"Alert is only used on the route moving to that route we can prevent "
-  },
-  {
-    id:5,
-    rating:8,
-    text:"Alert is only used on the route moving to that route we can prevent "
-  },
-  )
+  const[feedback, setFeedback] = useState(FeedbackData)
   return (
    <>
    <Header />
--- exercises01/src/data/FeedbackData.js
+++ exercises01/src/data/FeedbackData.js
@@ -1,7 +1,31 @@
 import React from 'react'
 
-export const FeedbackData = () => {
-  return (
-    <div>FeedbackData</div>
-  )
-}
+const FeedbackData = [
+
+    {
+        id:1,
+        rating:10,
+        text:"Alert is only used on the route moving to that route we can prevent "
+      },
+      {
+        id:2,
+        rating:6,
+        text:"Alert is only used on the route moving to that route we can prevent "
+      },
+      {
+        id:3,
+        rating:0,
+        text:"Alert is only used on the route moving to that route we can prevent "
+      },
+      {
+        id:4,
+        rating:7,
+        text:"Alert is only used on the route moving to that route we can prevent "
+      },
+      {
+        id:5,
+        rating:8,
+        text:"Alert is only used on the route moving to that route we can prevent "
+      },
+]
+export default FeedbackData;
