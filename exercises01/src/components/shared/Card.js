d6572f3d8d2939f3a6617feaee7bc1debff09dbf
unknown <vladancupric@gmail.com>
2022-08-17 12:28:39 +0200

    New commits

--- exercises01/src/components/shared/Card.js
+++ exercises01/src/components/shared/Card.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
-function Card({children,reverse}) {
+function Card({ children, reverse }) {
   return (
-    <div className={`card ${reverse && 'reverse'}`}> {children}</div>
-  )
+    // <div className={`card ${reverse && 'reverse'}`}> {children}</div>
+    <div
+      className="card"
+      style={{ backgroundColor: reverse ? "rgba(0,0,0,0.4)" : "#fff" }}
+    >
+      {children}
+    </div>
+  );
 }
 
 export default Card;
