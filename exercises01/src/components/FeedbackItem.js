5e17e91133779eb0dd940f14653650f516f39675
unknown <vladancupric@gmail.com>
2022-08-17 13:02:41 +0200

    New commits

--- exercises01/src/components/FeedbackItem.js
+++ exercises01/src/components/FeedbackItem.js
@@ -6,6 +6,9 @@ import {FaTimes} from 'react-icons/fa';
 function FeedbackItem({item}) {
 
     const [rating, setRating] = useState(7);
+    const handleClick = ()=> {
+      console.log(123);
+    }
     
     // const [text, setText] = useState("This is an example of a feedback ")
     // // const handleClick =()=>{
@@ -17,7 +20,7 @@ function FeedbackItem({item}) {
     return (
     <Card >
         <div className="num-display" >{item.rating}</div>
-        <button onAuxClick={()=>console.log(123)} className='close'>
+        <button onClick={()=>handleClick()} className='close'>
           <FaTimes color="purple" />
         </button>
         <div className="text-display">{item.text}</div>
