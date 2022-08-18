f40c0b8311a0dfcca3f232860d5d4a1db88557fb
unknown <vladancupric@gmail.com>
2022-08-18 18:53:55 +0200

    New commits

--- exercises01/src/components/FeedbackForm.js
+++ exercises01/src/components/FeedbackForm.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import Card from './shared/Card';
+import React from "react";
+import { useState } from "react";
+import Card from "./shared/Card";
 
-function Feedbackform() {
+function FeedbackForm() {
+    const [text, setText] = useState("")
+    const handleTextchange = (e)=>{
+        console.log(e.target.value);
+    }
   return (
-    <div>FeedbackForm</div>
+   <Card>
+    <form>
+        <h2>
+            How would you rate your service with us?</h2>
+            <div className="input-group">
+                <input onChange={handleTextchange} type="text" placeholder="Write a review" />
+                <button type="submit">Send</button>
+            </div>
+    </form>
+   </Card>
   )
 }
 
-export default Feedbackform;
+export default FeedbackForm;
