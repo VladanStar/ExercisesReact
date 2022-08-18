e5c5d69898ae023fe72b832e48150b4842090c8a
unknown <vladancupric@gmail.com>
2022-08-18 18:55:51 +0200

    Add handleTextXhange function

--- exercises01/src/components/FeedbackForm.js
+++ exercises01/src/components/FeedbackForm.js
@@ -3,22 +3,26 @@ import { useState } from "react";
 import Card from "./shared/Card";
 
 function FeedbackForm() {
-    const [text, setText] = useState("")
-    const handleTextchange = (e)=>{
-        console.log(e.target.value);
-    }
+  const [text, setText] = useState("");
+  const handleTextChange = (e) => {
+    console.log(e.target.value);
+  };
   return (
-   <Card>
-    <form>
-        <h2>
-            How would you rate your service with us?</h2>
-            <div className="input-group">
-                <input onChange={handleTextchange} type="text" placeholder="Write a review" />
-                <button type="submit">Send</button>
-            </div>
-    </form>
-   </Card>
-  )
+    <Card>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={text}
+          />
+          <button type="submit">Send</button>
+        </div>
+      </form>
+    </Card>
+  );
 }
 
 export default FeedbackForm;
