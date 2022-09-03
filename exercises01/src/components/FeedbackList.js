224b24b02cf5463ffbae2ff0c5989177c5fce91a
unknown <vladancupric@gmail.com>
2022-09-03 20:54:11 +0200

    Add spinner loading

--- exercises01/src/components/FeedbackList.js
+++ exercises01/src/components/FeedbackList.js
@@ -1,21 +1,25 @@
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
-import {useContext} from "react";
+import { useContext } from "react";
 import FeedbackContext from "./context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function FeedbackList() {
-  const {feedback, isLoading} = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
   // const {handleDelete} = useContext(FeedbackContext);
   console.log(feedback);
   if (!isLoading && (!feedback || feedback.lenth === 0)) {
-    return <p>No Feedback Yet</p>;
+    return<Spinner />
   }
-  return isLoading ? <h3>Loading...</h3>: ( <div className="feedback-list">
-  {feedback.map((item) => (
-    <FeedbackItem key={item.id} item={item}  />
-  ))}
-</div>)
- 
+  return isLoading ? (
+    <h3>Loading...</h3>
+  ) : (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} />
+      ))}
+    </div>
+  );
 }
 
 export default FeedbackList;
